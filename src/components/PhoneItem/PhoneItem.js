0120a2071513f8965affa45c4473ff6b0695d928
Adrián Moreno Navarro <adrian.moreno.contractor@bbva.com>
2020-02-02 20:14:20 +0100

    sass included

--- src/components/PhoneItem/PhoneItem.js
+++ src/components/PhoneItem/PhoneItem.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
-import './PhoneItem.css'
+import './PhoneItem.scss'
 
 class PhoneItem extends React.Component {
-    render() {
-        const { img, model } = this.props;
-        return (
-            <div className="phone-item-container" data-test="phoneItem">
-                <img
-                    className="phone-image"
-                    src={img}
-                    alt={model}
-                />
-                <h5 className="phone-model">{model}</h5>
-            </div>
-        )
-    }
+  render() {
+    const { img, model } = this.props;
+    return (
+      <div className="phone-item__container" data-test="phoneItem">
+        <img
+          className="phone-item__image"
+          src={img}
+          alt={model}
+        />
+        <h5 className="phone-item__model">{model}</h5>
+      </div>
+    )
+  }
 }
   
 export default PhoneItem;
